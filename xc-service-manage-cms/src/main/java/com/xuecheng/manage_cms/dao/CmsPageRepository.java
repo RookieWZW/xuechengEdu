6ffca8337df8e1164e6d582551c488fa594766d8
RookieWangZhiWei <RookieWangZhiWei@163.com>
2019-02-20 23:56:46 +0800

    first commit

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -0,0 +1,23 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Created by RookieWangZhiWei on 2019/2/20.
+ */
+public interface CmsPageRepository extends MongoRepository<CmsPage, String> {
+
+
+    CmsPage findByPageName(String pageName);
+
+    CmsPage findByPageNameAndPageType(String pageName, String pageType);
+
+    int countBySiteIdAndPageType(String siteId, String pageType);
+
+    Page<CmsPage> findBySiteIdAndPageType(String siteId, String pageType, Pageable pageable);
+
+
+}
---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -0,0 +1,48 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by RookieWangZhiWei on 2019/2/20.
+ */
+@Service
+public class PageService {
+
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
+
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
+        if (queryPageRequest == null) {
+            queryPageRequest = new QueryPageRequest();
+        }
+        if (page <= 0) {
+            page = 1;
+        }
+        page = page - 1;
+        if (size <= 0) {
+            size = 20;
+        }
+
+        Pageable pageable =  PageRequest.of(page, size);
+
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+
+        QueryResult<CmsPage> cmsPageQueryResult = new QueryResult<CmsPage>();
+
+        cmsPageQueryResult.setList(all.getContent());
+
+        cmsPageQueryResult.setTotal(all.getTotalElements());
+
+        return new QueryResponseResult(CommonCode.SUCCESS, cmsPageQueryResult);
+    }
+}
